01f30d80cf85ed55d57c2993eeea2b432f935b19
Jan Podmolík <wexly.ue@gmail.com>
2020-08-02 11:36:53 +0200

    Initial Commit

---
+++ Hybse/HybseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HybseApp.swift
+//  Hybse
+//
+//  Created by Johana Šlechtová on 02/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct HybseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
